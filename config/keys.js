9c1bf1c8b44e0dc9e4d52f3b81433e355a5c4aca
Chamika Ravinda <chamikaravinda@gmail.com>
2020-10-06 16:07:05 +0530

    add key file

---
+++ config/keys.js
@@ -0,0 +1,16 @@
+//add this file to .gitignore
+
+module.exports = {
+  google: {
+    clientID:
+      "987471008844-9ff1270irdq9tqvfu8ceqhpq4t4gctcn.apps.googleusercontent.com",
+    clientSecret: "A0m7xTN6P4NtdZ3jWSx1qof5",
+  },
+  mongodb: {
+    dbURI:
+      "mongodb+srv://chamikaravinda:Iejnf7sbGjdb9c3H@cluster0.eus6c.azure.mongodb.net/OAuth?retryWrites=true&w=majority",
+  },
+  session: {
+    cookieKey: "gfsddf34jnfs54kfgsd563fde543a",
+  },
+};
